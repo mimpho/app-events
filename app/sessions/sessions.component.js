4ca380b19108b98bd4db9992f34ef11efa557ef9
Marcos <mimpho@gmail.com>
2016-09-02 01:43:13 +0200

    push y pop locations

--- app/sessions/sessions.component.js
+++ app/sessions/sessions.component.js
@@ -7,14 +7,30 @@ angular.
     templateUrl: 'sessions/sessions.template.html',
     controller: ['$http', '$state', function SessionsController($http, $state) {
       var self = this;
+      self.locations = null;
+      
       self.orderProp = '-sessions.date';
+
+      self.pushLocation = function pushLocation(index) {
+        if (self.locations[index] < self.event.sessions[index].availability)
+          self.locations[index]++;
+      };
+      self.popLocation = function pushLocation(index) {
+        if (self.locations[index] > 0)
+          self.locations[index]--;
+      };
+
       $http.
         get('assets/data/event-info-'+$state.params.eventid+'.json').
         then(function successCallback(response) {
           self.event = response.data;
+          self.locations = new Array(self.event.sessions.length);
+
         }, function errorCallback(response) {
           self.event = null;
         });
+      
       $(document).foundation();
+      
     }]
   });
