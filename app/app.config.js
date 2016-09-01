c068a63b5e01442e32b5df3d4e2c4d9d98eb3f41
Marcos <mimpho@gmail.com>
2016-09-01 12:55:58 +0200

    TODOs ui-router

--- app/app.config.js
+++ app/app.config.js
@@ -1,5 +1,7 @@
 /*
-  TODO: Mirar explicación en http://www.ng-newsletter.com/posts/angular-ui-router.html
+  TODOs
+  Este ejemplo sencillo funciona: http://plnkr.co/edit/NpzQsxYGofswWQUBGthR?p=preview
+  Mirar explicación: http://www.ng-newsletter.com/posts/angular-ui-router.html
 */
 'use strict';
 
